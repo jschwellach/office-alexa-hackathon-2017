31468b18a20fa5e778cbd9c074cc1d4a77fe2441
Ricardo Mota <ricardosllm@gmail.com>
2017-11-30 12:53:06 -0800

    Save state to ddb

--- office-alexa-reinvent-2017/handler.js
+++ office-alexa-reinvent-2017/handler.js
@@ -20,143 +20,199 @@ const Alexa = require('alexa-sdk');
 const APP_ID = "amzn1.ask.skill.42d881a4-20f3-4fe4-897b-3f063403a463";
 
 const languageStrings = {
-    'en': {
-        translation: {
-            FACTS: [
-                'A year on Mercury is just 88 days long.',
-                'Despite being farther from the Sun, Venus experiences higher temperatures than Mercury.',
-                'Venus rotates anti-clockwise, possibly because of a collision in the past with an asteroid.',
-                'On Mars, the Sun appears about half the size as it does on Earth.',
-                'Earth is the only planet not named after a god.',
-                'Jupiter has the shortest day of all the planets.',
-                'The Milky Way galaxy will collide with the Andromeda Galaxy in about 5 billion years.',
-                'The Sun contains 99.86% of the mass in the Solar System.',
-                'The Sun is an almost perfect sphere.',
-                'A total solar eclipse can happen once every 1 to 2 years. This makes them a rare event.',
-                'Saturn radiates two and a half times more energy into space than it receives from the sun.',
-                'The temperature inside the Sun can reach 15 million degrees Celsius.',
-                'The Moon is moving approximately 3.8 cm away from our planet every year.',
-            ],
-            SKILL_NAME: 'Space Facts',
-            GET_FACT_MESSAGE: "Here's your fact: ",
-            HELP_MESSAGE: 'You can say tell me a space fact, or, you can say exit... What can I help you with?',
-            HELP_REPROMPT: 'What can I help you with?',
-            STOP_MESSAGE: 'Goodbye!',
-            CHECK_IN: 'Checked in!',
-        },
+  'en': {
+    translation: {
+      FACTS: [
+        'A year on Mercury is just 88 days long.',
+        'Despite being farther from the Sun, Venus experiences higher temperatures than Mercury.',
+        'Venus rotates anti-clockwise, possibly because of a collision in the past with an asteroid.',
+        'On Mars, the Sun appears about half the size as it does on Earth.',
+        'Earth is the only planet not named after a god.',
+        'Jupiter has the shortest day of all the planets.',
+        'The Milky Way galaxy will collide with the Andromeda Galaxy in about 5 billion years.',
+        'The Sun contains 99.86% of the mass in the Solar System.',
+        'The Sun is an almost perfect sphere.',
+        'A total solar eclipse can happen once every 1 to 2 years. This makes them a rare event.',
+        'Saturn radiates two and a half times more energy into space than it receives from the sun.',
+        'The temperature inside the Sun can reach 15 million degrees Celsius.',
+        'The Moon is moving approximately 3.8 cm away from our planet every year.',
+      ],
+      SKILL_NAME: 'Space Facts',
+      GET_FACT_MESSAGE: "Here's your fact: ",
+      HELP_MESSAGE: 'You can say tell me a space fact, or, you can say exit... What can I help you with?',
+      HELP_REPROMPT: 'What can I help you with?',
+      STOP_MESSAGE: 'Goodbye!',
+      CHECK_IN: 'Checked in!',
     },
-    'en-US': {
-        translation: {
-            FACTS: [
-                'A year on Mercury is just 88 days long.',
-                'Despite being farther from the Sun, Venus experiences higher temperatures than Mercury.',
-                'Venus rotates counter-clockwise, possibly because of a collision in the past with an asteroid.',
-                'On Mars, the Sun appears about half the size as it does on Earth.',
-                'Earth is the only planet not named after a god.',
-                'Jupiter has the shortest day of all the planets.',
-                'The Milky Way galaxy will collide with the Andromeda Galaxy in about 5 billion years.',
-                'The Sun contains 99.86% of the mass in the Solar System.',
-                'The Sun is an almost perfect sphere.',
-                'A total solar eclipse can happen once every 1 to 2 years. This makes them a rare event.',
-                'Saturn radiates two and a half times more energy into space than it receives from the sun.',
-                'The temperature inside the Sun can reach 15 million degrees Celsius.',
-                'The Moon is moving approximately 3.8 cm away from our planet every year.',
-            ],
-            SKILL_NAME: 'American Space Facts',
-        },
+  },
+  'en-US': {
+    translation: {
+      FACTS: [
+        'A year on Mercury is just 88 days long.',
+        'Despite being farther from the Sun, Venus experiences higher temperatures than Mercury.',
+        'Venus rotates counter-clockwise, possibly because of a collision in the past with an asteroid.',
+        'On Mars, the Sun appears about half the size as it does on Earth.',
+        'Earth is the only planet not named after a god.',
+        'Jupiter has the shortest day of all the planets.',
+        'The Milky Way galaxy will collide with the Andromeda Galaxy in about 5 billion years.',
+        'The Sun contains 99.86% of the mass in the Solar System.',
+        'The Sun is an almost perfect sphere.',
+        'A total solar eclipse can happen once every 1 to 2 years. This makes them a rare event.',
+        'Saturn radiates two and a half times more energy into space than it receives from the sun.',
+        'The temperature inside the Sun can reach 15 million degrees Celsius.',
+        'The Moon is moving approximately 3.8 cm away from our planet every year.',
+      ],
+      SKILL_NAME: 'American Space Facts',
     },
-    'en-GB': {
-        translation: {
-            FACTS: [
-                'A year on Mercury is just 88 days long.',
-                'Despite being farther from the Sun, Venus experiences higher temperatures than Mercury.',
-                'Venus rotates anti-clockwise, possibly because of a collision in the past with an asteroid.',
-                'On Mars, the Sun appears about half the size as it does on Earth.',
-                'Earth is the only planet not named after a god.',
-                'Jupiter has the shortest day of all the planets.',
-                'The Milky Way galaxy will collide with the Andromeda Galaxy in about 5 billion years.',
-                'The Sun contains 99.86% of the mass in the Solar System.',
-                'The Sun is an almost perfect sphere.',
-                'A total solar eclipse can happen once every 1 to 2 years. This makes them a rare event.',
-                'Saturn radiates two and a half times more energy into space than it receives from the sun.',
-                'The temperature inside the Sun can reach 15 million degrees Celsius.',
-                'The Moon is moving approximately 3.8 cm away from our planet every year.',
-            ],
-            SKILL_NAME: 'British Space Facts',
-        },
+  },
+  'en-GB': {
+    translation: {
+      FACTS: [
+        'A year on Mercury is just 88 days long.',
+        'Despite being farther from the Sun, Venus experiences higher temperatures than Mercury.',
+        'Venus rotates anti-clockwise, possibly because of a collision in the past with an asteroid.',
+        'On Mars, the Sun appears about half the size as it does on Earth.',
+        'Earth is the only planet not named after a god.',
+        'Jupiter has the shortest day of all the planets.',
+        'The Milky Way galaxy will collide with the Andromeda Galaxy in about 5 billion years.',
+        'The Sun contains 99.86% of the mass in the Solar System.',
+        'The Sun is an almost perfect sphere.',
+        'A total solar eclipse can happen once every 1 to 2 years. This makes them a rare event.',
+        'Saturn radiates two and a half times more energy into space than it receives from the sun.',
+        'The temperature inside the Sun can reach 15 million degrees Celsius.',
+        'The Moon is moving approximately 3.8 cm away from our planet every year.',
+      ],
+      SKILL_NAME: 'British Space Facts',
     },
-    'de': {
-        translation: {
-            FACTS: [
-                'Ein Jahr dauert auf dem Merkur nur 88 Tage.',
-                'Die Venus ist zwar weiter von der Sonne entfernt, hat aber höhere Temperaturen als Merkur.',
-                'Venus dreht sich entgegen dem Uhrzeigersinn, möglicherweise aufgrund eines früheren Zusammenstoßes mit einem Asteroiden.',
-                'Auf dem Mars erscheint die Sonne nur halb so groß wie auf der Erde.',
-                'Die Erde ist der einzige Planet, der nicht nach einem Gott benannt ist.',
-                'Jupiter hat den kürzesten Tag aller Planeten.',
-                'Die Milchstraßengalaxis wird in etwa 5 Milliarden Jahren mit der Andromeda-Galaxis zusammenstoßen.',
-                'Die Sonne macht rund 99,86 % der Masse im Sonnensystem aus.',
-                'Die Sonne ist eine fast perfekte Kugel.',
-                'Eine Sonnenfinsternis kann alle ein bis zwei Jahre eintreten. Sie ist daher ein seltenes Ereignis.',
-                'Der Saturn strahlt zweieinhalb mal mehr Energie in den Weltraum aus als er von der Sonne erhält.',
-                'Die Temperatur in der Sonne kann 15 Millionen Grad Celsius erreichen.',
-                'Der Mond entfernt sich von unserem Planeten etwa 3,8 cm pro Jahr.',
-            ],
-            SKILL_NAME: 'Weltraumwissen auf Deutsch',
-            GET_FACT_MESSAGE: 'Hier sind deine Fakten: ',
-            HELP_MESSAGE: 'Du kannst sagen, „Nenne mir einen Fakt über den Weltraum“, oder du kannst „Beenden“ sagen... Wie kann ich dir helfen?',
-            HELP_REPROMPT: 'Wie kann ich dir helfen?',
-            STOP_MESSAGE: 'Auf Wiedersehen!',
-        },
+  },
+  'de': {
+    translation: {
+      FACTS: [
+        'Ein Jahr dauert auf dem Merkur nur 88 Tage.',
+        'Die Venus ist zwar weiter von der Sonne entfernt, hat aber höhere Temperaturen als Merkur.',
+        'Venus dreht sich entgegen dem Uhrzeigersinn, möglicherweise aufgrund eines früheren Zusammenstoßes mit einem Asteroiden.',
+        'Auf dem Mars erscheint die Sonne nur halb so groß wie auf der Erde.',
+        'Die Erde ist der einzige Planet, der nicht nach einem Gott benannt ist.',
+        'Jupiter hat den kürzesten Tag aller Planeten.',
+        'Die Milchstraßengalaxis wird in etwa 5 Milliarden Jahren mit der Andromeda-Galaxis zusammenstoßen.',
+        'Die Sonne macht rund 99,86 % der Masse im Sonnensystem aus.',
+        'Die Sonne ist eine fast perfekte Kugel.',
+        'Eine Sonnenfinsternis kann alle ein bis zwei Jahre eintreten. Sie ist daher ein seltenes Ereignis.',
+        'Der Saturn strahlt zweieinhalb mal mehr Energie in den Weltraum aus als er von der Sonne erhält.',
+        'Die Temperatur in der Sonne kann 15 Millionen Grad Celsius erreichen.',
+        'Der Mond entfernt sich von unserem Planeten etwa 3,8 cm pro Jahr.',
+      ],
+      SKILL_NAME: 'Weltraumwissen auf Deutsch',
+      GET_FACT_MESSAGE: 'Hier sind deine Fakten: ',
+      HELP_MESSAGE: 'Du kannst sagen, „Nenne mir einen Fakt über den Weltraum“, oder du kannst „Beenden“ sagen... Wie kann ich dir helfen?',
+      HELP_REPROMPT: 'Wie kann ich dir helfen?',
+      STOP_MESSAGE: 'Auf Wiedersehen!',
     },
+  },
 };
 
-const handlers = {
-    'LaunchRequest': function () {
-        this.emit('GetFact');
-    },
-    'CheckIn': function () {
-        const speechOutput = this.t('CHECK_IN');
-      console.log('event: ', this.event);
-      console.log('user: ', this.event.session.user);
-      var user = this.event.session.user;
-      var userId = user.userId;
-        var userString = users[userId];
-        this.emit(':tellWithCard', speechOutput + userString);
-    },
-    'GetFact': function () {
-        // Get a random space fact from the space facts list
-        // Use this.t() to get corresponding language data
-        const factArr = this.t('FACTS');
-        const factIndex = Math.floor(Math.random() * factArr.length);
-        const randomFact = factArr[factIndex];
 
-        // Create speech output
-        const speechOutput = this.t('GET_FACT_MESSAGE') + randomFact;
-        this.emit(':tellWithCard', speechOutput, this.t('SKILL_NAME'), randomFact);
-    },
-    'AMAZON.HelpIntent': function () {
-        const speechOutput = this.t('HELP_MESSAGE');
-        const reprompt = this.t('HELP_MESSAGE');
-        this.emit(':ask', speechOutput, reprompt);
-    },
-    'AMAZON.CancelIntent': function () {
-        this.emit(':tell', this.t('STOP_MESSAGE'));
-    },
-    'AMAZON.StopIntent': function () {
-        this.emit(':tell', this.t('STOP_MESSAGE'));
-    },
+// Load the AWS SDK for Node.js
+var AWS = require('aws-sdk');
+// Set the region
+AWS.config.update({region: 'REGION'});
+
+// Create the DynamoDB service object
+var ddb = new AWS.DynamoDB({apiVersion: '2012-10-08'});
+
+
+const handlers = {
+  'LaunchRequest': function () {
+    this.emit('GetFact');
+  },
+  'CheckIn': function () {
+    const speechOutput = this.t('CHECK_IN');
+    console.log('event: ', this.event);
+    console.log('user: ', this.event.session.user);
+    var user = this.event.session.user;
+    var userId = user.userId;
+    var userString = users[userId];
+    // Save checkin state to dynamo
+    var params = {
+      TableName: process.env.DYNAMODB_TABLE,
+      Item: {
+        'name' : userString,
+        'checked_in' : true
+      }
+    };
+
+    // Call DynamoDB to add the item to the table
+    ddb.putItem(params, function(err, data) {
+      if (err) {
+        console.log("Error", err);
+      } else {
+        console.log("Success", data);
+      }
+    });
+    this.emit(':tellWithCard', speechOutput + userString);
+  },
+  'meetingRequest': function () {
+    // Assuming Tom is the caller
+    // John is the target
+    var caller = 'Tom';
+    var target = 'John';
+    var params = {
+      Key: {
+        "name": {
+          S: target
+        }
+      },
+      TableName: process.env.DYNAMODB_TABLE,
+    };
+    dynamodb.getItem(params, function(err, data) {
+      if (err) {console.log(err, err.stack)} // an error occurred
+      else {
+        console.log(data);           // successful response
+        //
+      }
+    });
+
+    console.log('event: ', this.event);
+    console.log('user: ', this.event.session.user);
+    var user = this.event.session.user;
+    var userId = user.userId;
+    var userString = users[userId];
+    // Save checkin state to dynamo
+    this.emit(':tellWithCard', speechOutput + userString);
+  },
+  'GetFact': function () {
+    // Get a random space fact from the space facts list
+    // Use this.t() to get corresponding language data
+    const factArr = this.t('FACTS');
+    const factIndex = Math.floor(Math.random() * factArr.length);
+    const randomFact = factArr[factIndex];
+
+    // Create speech output
+    const speechOutput = this.t('GET_FACT_MESSAGE') + randomFact;
+    this.emit(':tellWithCard', speechOutput, this.t('SKILL_NAME'), randomFact);
+  },
+  'AMAZON.HelpIntent': function () {
+    const speechOutput = this.t('HELP_MESSAGE');
+    const reprompt = this.t('HELP_MESSAGE');
+    this.emit(':ask', speechOutput, reprompt);
+  },
+  'AMAZON.CancelIntent': function () {
+    this.emit(':tell', this.t('STOP_MESSAGE'));
+  },
+  'AMAZON.StopIntent': function () {
+    this.emit(':tell', this.t('STOP_MESSAGE'));
+  },
 };
 
 exports.handler = function (event, context) {
-    const alexa = Alexa.handler(event, context);
-    console.log("event:");
-    console.log(event);
-    console.log("context");
-    console.log(context);
-    alexa.APP_ID = APP_ID;
-    // To enable string internationalization (i18n) features, set a resources object.
-    alexa.resources = languageStrings;
-    alexa.registerHandlers(handlers);
-    alexa.execute();
+  const alexa = Alexa.handler(event, context);
+  console.log("event:");
+  console.log(event);
+  console.log("context");
+  console.log(context);
+  alexa.APP_ID = APP_ID;
+  // To enable string internationalization (i18n) features, set a resources object.
+  alexa.resources = languageStrings;
+  alexa.registerHandlers(handlers);
+  alexa.execute();
 };
